d3d0a5f14236c071407d366fade4425fb8d25583
HélènRuffieux <helene.ruffieux@mrc-bsu.cam.ac.uk>
2022-07-04 21:56:26 +0200

    Added cosmetic changes.

--- R/summarise_output.R
+++ R/summarise_output.R
@@ -106,8 +106,8 @@ summary.atlasqtl <- function(object, thres, fdr_adjust = FALSE, full_summary = T
                thres, ":\n"))
     cat("-----------------------------\n")
     cat(paste0("\nNb of pairwise (predictor-response) associations: ", nb_pairwise), "\n")
-    cat(paste0("\nNb of predictors associated with > 1 response ", 
-               "(active \npredictors): ", sum(rs_thres>0)), "\n")
+    cat(paste0("\nNb of predictors associated with at least one response \n", 
+               "(active predictors): ", sum(rs_thres>0)), "\n")
     cat("\nHotspot sizes (nb of responses associated with each \nactive predictors):\n")
     print(summary(rs_thres[rs_thres>0])) 
     
