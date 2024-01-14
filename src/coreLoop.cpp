485985b467a5ec5b249a5baf15aaa8b47a7afaa5
Hélène Ruffieux <helene.ruffieux@mrc-bsu.cam.ac.uk>
2024-01-14 19:37:31 +0100

    Added minor change in C++ version.

--- src/coreLoop.cpp
+++ src/coreLoop.cpp
@@ -30,29 +30,29 @@ void coreDualLoop(const MapMat cp_X,
                   const MapArr1D tau_vb,
                   const MapArr1D shuffled_ind,
                   const double c = 1) {
-
+  
   const Arr1D cst = -(log_tau_vb + log_sig2_inv_vb + log(sig2_beta_vb) )/ 2;
-
+  
   for (int i = 0; i < cp_X.cols(); ++i) {
-
+    
     int j = shuffled_ind[i];
-
+    
     // cout << cp_betaX_X.rows();
-
+    
     cp_betaX_X.noalias() -= m1_beta.row(j).transpose() * cp_X.row(j);
-
+    
     mu_beta_vb.row(j) = c * sig2_beta_vb * tau_vb * (cp_Y_X.col(j) - cp_betaX_X.col(j)).array();
-
+    
     gam_vb.row(j) = exp(-logOnePlusExp(c * (log_1_min_Phi_theta_plus_zeta.row(j) -
       log_Phi_theta_plus_zeta.row(j) - mu_beta_vb.row(j).square() / (2 * sig2_beta_vb.transpose()) +
       cst.transpose())));
-
+    
     m1_beta.row(j) = mu_beta_vb.row(j) * gam_vb.row(j);
-
+    
     cp_betaX_X.noalias() += m1_beta.row(j).transpose() * cp_X.row(j);
-
+    
   }
-
+  
 }
 
 
@@ -73,34 +73,34 @@ void coreDualMisLoop(const MapMat cp_X,
                      const MapArr1D tau_vb,
                      const MapArr1D shuffled_ind,
                      const double c = 1) {
-
+  
   const Arr1D cst = -(log_tau_vb + log_sig2_inv_vb)/ 2;
   int q = cp_Y_X.rows();
-
+  
   for (int i = 0; i < cp_X.cols(); ++i) {
-   
-     int j = shuffled_ind[i];
-   
-     cp_betaX_X.noalias() -= m1_beta.row(j).transpose() * cp_X.row(j);
+    
+    int j = shuffled_ind[i];
+    
+    cp_betaX_X.noalias() -= m1_beta.row(j).transpose() * cp_X.row(j);
     for (int k = 0; k < q; ++k) {
-      MapMat cp_X_rm_k = as<MapMat>(cp_X_rm[k]);
-      cp_betaX_X.row(k) += m1_beta(j, k) * cp_X_rm_k.row(j);
+      // MapMat cp_X_rm_k = as<MapMat>(cp_X_rm[k]);
+      cp_betaX_X.row(k) += m1_beta(j, k) * as<MapMat>(cp_X_rm[k]).row(j);
     }
     mu_beta_vb.row(j) = c * sig2_beta_vb.row(j).transpose() * tau_vb *
       (cp_Y_X.col(j) - cp_betaX_X.col(j)).array();
-
+    
     gam_vb.row(j) = exp(-logOnePlusExp(c * (log_1_min_Phi_theta_plus_zeta.row(j) -
       log_Phi_theta_plus_zeta.row(j) - mu_beta_vb.row(j).square() / (2 * sig2_beta_vb.row(j)) -
       log(sig2_beta_vb.row(j)) / 2 + cst.transpose())));
-
+    
     m1_beta.row(j) = mu_beta_vb.row(j) * gam_vb.row(j);
-
+    
     cp_betaX_X.noalias() += m1_beta.row(j).transpose() * cp_X.row(j);
     for (int k = 0; k < q; ++k) {
-      MapMat cp_X_rm_k = as<MapMat>(cp_X_rm[k]);
-      cp_betaX_X.row(k) -= m1_beta(j, k) * cp_X_rm_k.row(j);
+      // MapMat cp_X_rm_k = as<MapMat>(cp_X_rm[k]);
+      cp_betaX_X.row(k) -= m1_beta(j, k) * as<MapMat>(cp_X_rm[k]).row(j);
     }
-
-   }
-
+    
+  }
+  
 }
